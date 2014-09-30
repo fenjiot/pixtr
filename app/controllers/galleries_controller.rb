17f0caaaf62619e723782a8446de616a91f98164
fenjiot <fenjiot@gmail.com>
2014-09-30 12:01:32 -0400

    Use rails resources method for routes

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,24 +1,15 @@
 class GalleriesController < ApplicationController
   def index
-    # We don't actually need render index becuase it till automatically render the method
-    # name index, so if it was called game it would render :game.
     @galleries = Gallery.all.order("name ASC")
-    render :index
   end 
 
   def new
-    # continuing comment above: don't need to render :new, since Rails
-    # knows to do render :method_name, in this case :new.
     @gallery = Gallery.new
-    # creating a new gallery unassociated with any row
   end
 
   def create
-    Gallery.create(
-      name: params[:gallery][:name],
-      description: params[:gallery][:description]
-    )
-    redirect_to "/"
+    gallery = Gallery.create(gallery_params)
+    redirect_to(gallery_path(gallery))
   end
 
   def show
@@ -31,17 +22,24 @@ class GalleriesController < ApplicationController
 
   def update
     gallery = Gallery.find(params[:id]) 
-    gallery.update(
-      name: params[:gallery][:name],
-      description: params[:gallery][:description]
-    )
+    gallery.update(gallery_params)
+
     redirect_to "/"
   end
 
   def destroy
     gallery = Gallery.find(params[:id])
     gallery.destroy
+
     redirect_to "/"
+  end
+
+
+  private
+
+  def gallery_params
+    # whitelists attributes
+    params.require(:gallery).permit(:name, :description)
   end
 
 end
