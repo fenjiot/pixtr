054265f474b1b37aaaae7bf1e5115d9f854973c7
fenjiot <fenjiot@gmail.com>
2014-10-01 11:12:15 -0400

    Users can create images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,4 +3,27 @@ class ImagesController < ApplicationController
     @gallery = Gallery.find(params[:gallery_id])
     @image = @gallery.images.find(params[:id])
   end
+
+  def new
+    @gallery = Gallery.find(params[:gallery_id])
+    @image = @gallery.images.new
+  end
+
+  def create
+    @gallery = Gallery.find(params[:gallery_id])
+    @image = @gallery.images.new(image_params)
+    @image.save(image_params)
+
+    redirect_to gallery_path(@gallery)
+  end
+
+
+  private
+
+  def image_params
+    params.
+      require(:image).
+      permit(:name, :url)
+  end
+
 end
