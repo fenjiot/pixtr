3e502a3c16eff421dfdd86cd6bf4b7e96bffb465
fenjiot <fenjiot@gmail.com>
2014-10-06 15:18:04 -0400

    Users can comment on images

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  def create
+    @image = Image.find(params[:image_id])
+    @comment = @image.comments.new(comment_params)
+    @gallery = @image.gallery
+    @comments = @image.comments.all
+
+    if @comment.save
+      redirect_to gallery_image_path(@gallery, @image)
+    else
+      render "images/show" 
+    end
+  end
+
+
+  private
+
+  def comment_params
+    params.
+      require(:comment).
+      permit(:body).merge(user_id: current_user.id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
 
   resource :session, only: [:new, :create, :destroy]
   root "galleries#index"
+
   resources :users, only: [:new, :create]
 
   resources :galleries do
     resources :images, only: [:show, :new, :create, :edit, :update]
+  end
+
+  resources :images, only: [] do
+    resources :comments, only: [:create]
   end
 
 end
