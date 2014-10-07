191748bafd0a6cd547ec22f7a84c62d5a06d77c1
fenjiot <fenjiot@gmail.com>
2014-10-07 14:38:56 -0400

    Allow creating and showing of groups

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
     resources :comments, only: [:create]
   end
 
-  resources :groups, only: [:index]
+  resources :groups, only: [:index, :new, :create, :show]
 end
