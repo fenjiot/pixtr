45fb39d1d5f1cab7064a17d9d5b518853d32e046
fenjiot <fenjiot@gmail.com>
2014-10-08 09:39:11 -0400

    Remove unnecessary self

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ActiveRecord::Base
   validates :password_digest, presence: true
 
   def membership_for(group)
-    self.group_memberships.where(group_id: group.id).first
+    group_memberships.where(group_id: group.id).first
   end
 end
